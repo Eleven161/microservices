75cab66f53e8be263ec7fb8210092bbdbe272481
Upasna <272709@ODCDPW.IC>
2023-02-06 19:50:44 +0530

    complete implementation of spring security,okta & oauth2

--- HotelService/src/main/java/com/example/HotelService/config/SecurityConfig.java
+++ HotelService/src/main/java/com/example/HotelService/config/SecurityConfig.java
@@ -1,24 +1,23 @@
-package com.example.UserService.config;
+package com.example.HotelService.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
-public class WebSecurityClass {
-	
-	@Bean
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+    @Bean
 	public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
-		
-		security.authorizeHttpRequests().anyRequest()
-		.authenticated().and().oauth2ResourceServer().jwt();
-		
-		return security.build();
-	}
-
+    	security.authorizeHttpRequests()
+    	.anyRequest().authenticated()
+    	.and().oauth2ResourceServer()
+    	.jwt();
+     
+     return security.build();
+    }
 }
